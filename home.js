819ddbd03848fe2475312f3be50956805e873749
joevibanks <45922488+joevibanks@users.noreply.github.com>
2020-07-04 02:14:38 +0100

    Add files via upload

--- home.js
+++ home.js
@@ -1,33 +1,41 @@
-// const inputTodo = document.getElementById("todo-text");
-// const inputButton = document.getElementById("btn");
-// const listedItem = document.getElementById("listed-item");
+const inputTodo = document.getElementById("todo-text");
+const inputButton = document.getElementById("btn");
+const listedItem = document.getElementById("listed-item");
 
 
-// inputButton.addEventListener("click", function addOn()
+inputButton.addEventListener("click", function addOn()
 
-// {
+{
 
-//     //event.preventDefault();
-    
-    
-//     const todoDiv = document.createElement("div");
-//     todoDiv.classList.add("todo");
-
-//     const newTodo = document.createElement("li");
-//     newTodo.innerText= inputTodo.value;
-//     newTodo.classList.add("todo-item");
-//     todoDiv.appendChild(newTodo);
-
-//      const completedButton = document.createElement("button");
-//      completedButton.innerHTML = '<i class="fas fa-check"></i>';
-//      todoDiv.appendChild(completedButton);
-
-//      const trashButton = document.createElement("button");
-//      trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-//      todoDiv.appendChild(trashButton);
+    //event.preventDefault();
+   const inputSpan = document.createElement("span");
+   inputSpan.innerText = inputTodo.value;
+   
+   
+   const todoDiv = document.createElement("div");
+   todoDiv.classList.add("todo");
+   
+   const completedButton = document.createElement("button");
+   completedButton.innerHTML = '<i class="fas fa-check"></i>';
+   
+   const trashButton = document.createElement("button");
+   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  
+   
+   todoDiv.appendChild(inputSpan);
+   todoDiv.appendChild(completedButton);
+   todoDiv.appendChild(trashButton);
      
-//      listedItem.appendChild(todoDiv);
-// console.log(todoDiv);
-// });
+   const newTodo = document.createElement("li");
+   newTodo.classList.add("todo-item");
+   newTodo.appendChild(todoDiv);
+   listedItem.appendChild(newTodo);
+    trashButton.addEventListener("click",function(e){
+        listedItem.removeChild(e.target.parentElement.closest(".todo-item"));
+       
+       
+    });
+    inputTodo.value = "";
+});
 
 
